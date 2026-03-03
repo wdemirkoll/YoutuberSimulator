ec2d691514d604ae7ebfeec76e581b551f8b4056
Abdüsselam Demirkol <wdemirkoll.dev@gmail.com>
2026-03-03 09:21:39 +0300

    menus created

---
+++ include/Menus.h
@@ -0,0 +1,7 @@
+#ifndef MENUS_H
+#define MENUS_H
+
+void Statistics(); // Displays player statistics
+void Main_Menu(); // Shows the main menu and handles user interaction
+
+#endif // MENUS_H
---
+++ src/Menus.cpp
@@ -0,0 +1,39 @@
+#include "Menus.h"
+#include "Variables.h"
+#include <string>
+#include <iostream>
+
+void Statistics(){
+    // Stripes for a stylish look ↓
+    std::cout << "---------------------------------------------\n";
+    
+    // Print the user's channel name to the screen ↓
+    std::cout << "Channel: " << Channel_Name << "\n";
+
+    // Print the user's money to the screen ↓
+    std::cout << "Money: $" << Money << "\n";
+    
+    // Stripes for a stylish look ↓
+    std::cout << "---------------------------------------------\n";
+
+    // Print the user's equipment quality to the screen ↓
+    std::cout << "Equipment Quality: " << Equipment_Quality << "x\n";
+
+    // Print the user's subs to the screen ↓
+    std::cout << "Subs: " << Subs << "\n";
+
+    // Print the user's views to the screen ↓
+    std::cout << "Views: " << Views << "\n";
+
+    // Print the user's total videos to the screen ↓
+    std::cout << "Total Videos: " << Total_Video << "\n";
+
+    // Stripes for a stylish look ↓
+    std::cout << "---------------------------------------------\n";
+}
+
+void Main_Menu(){
+    std::cout << "1) Upload Video\n";
+    std::cout << "2) Upgrade equipment quality\n";
+    std::cout << "0) Exit\n";
+}
