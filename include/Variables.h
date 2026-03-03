0fd76968fd89f4d6a3f51f80b2c0fde77cecd443
Abdüsselam Demirkol <wdemirkoll.dev@gmail.com>
2026-03-03 08:44:45 +0300

    variables created

---
+++ include/Variables.h
@@ -0,0 +1,15 @@
+#ifndef VARIABLES_H
+#define VARIABLES_H
+#include <string>
+
+extern std::string Channel_Name; // User's channel name (nickname)
+
+extern double Money; // User's current money amount
+extern double Subs; // User's total subscriptions
+extern double Views; // User's total views
+
+extern double Equipment_Quality; // User's equipment quality
+
+extern int Total_Video; // User's total videos
+
+#endif // VARIABLES_H
---
+++ src/Variables.cpp
@@ -0,0 +1,12 @@
+#include "Variables.h"
+#include <string>
+
+std::string Channel_Name = ""; // User's channel name (initially empty)
+
+double Money = 0; // User's current money amount
+double Subs = 0; // User's total subscriptions
+double Views = 0; // User's total views
+
+double Equipment_Quality = 1.5; // User's equipment quality
+
+int Total_Video = 0; // User's total videos
